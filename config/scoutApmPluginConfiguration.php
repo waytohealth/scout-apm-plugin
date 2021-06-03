7ad02a968315cad16b2ed8d52cc778637ee73216
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2021-06-03 09:11:38 -0400

    Install phpcs and apply most fixes

---
+++ config/scoutApmPluginConfiguration.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Log\NullLogger;
+use Scoutapm\Agent;
+use Scoutapm\Config;
+use Scoutapm\Events\Span\SpanReference;
+use Scoutapm\ScoutApmAgent;
+
+/**
+ * This listens to various framework events and allows scout APM to hook into those as appropriate.
+ *
+ * @link  https://symfony.com/legacy/doc/reference/1_4/en/15-Events#chapter_15_sub_context_load_factories
+ * has details on the specific events to hook into
+ */
+class scoutApmPluginConfiguration extends sfPluginConfiguration
+{
+    /** @var ScoutApmAgent */
+    private $agent;
+
+    private function createAgent(): Agent
+    {
+        $agent = Agent::fromConfig(
+            new Config(), // use environment variables
+            new NullLogger() // TODO figure out a PSR-3 logger other than this
+        );
+        // If the core agent is not already running, this will download and run it (from /tmp by default)
+        $agent->connect();
+
+        return $agent;
+    }
+
+    public function initialize(): void
+    {
+        $this->agent = $this->createAgent();
+
+        $this->dispatcher->connect(
+            'doctrine.configure_connection',
+            function (sfEvent $event): void {
+                $connection = $event['connection'];
+                assert($connection instanceof Doctrine_Connection);
+
+                $profiler = new ScoutApmDoctrineListener($this->agent);
+
+                $connection->addListener($profiler, 'scout_apm_profiler');
+            }
+        );
+        $this->dispatcher->connect(
+            'command.pre_command',
+            function (sfEvent $event): void {
+                $span = $this->agent->startSpan(
+                    sprintf(
+                        '%s/%s',
+                        SpanReference::INSTRUMENT_JOB,
+                        get_class($event->getSubject())
+                    )
+                );
+                if (! $span) {
+                    return;
+                }
+
+                $span->tag('command.parameters', $event->getParameters());
+            }
+        );
+        $this->dispatcher->connect(
+            'command.post_command',
+            function (sfEvent $event): void {
+                $this->agent->stopSpan();
+                $this->agent->send();
+            }
+        );
+
+        $requestListener = new ScoutApmRequestListener($this->agent);
+        $requestListener->connect($this->dispatcher);
+    }
+}
--- lib/ScoutApmDoctrineListener.php
+++ lib/ScoutApmDoctrineListener.php
@@ -1,59 +1,60 @@
 <?php
 
+declare(strict_types=1);
 
 use Scoutapm\Events\Span\SpanReference;
 use Scoutapm\ScoutApmAgent;
 
 class ScoutApmDoctrineListener extends Doctrine_EventListener
 {
-  /** @var ScoutApmAgent */
-  private $agent;
-  /** @var SpanReference|null */
-  private $currentSpan;
+    /** @var ScoutApmAgent */
+    private $agent;
+    /** @var SpanReference|null */
+    private $currentSpan;
 
-  public function __construct(ScoutApmAgent $agent)
-  {
-    $this->agent = $agent;
-  }
-
-  public function preFetch(Doctrine_Event $event)
-  {
-    $this->startQuery('SQL/query', $event->getQuery(), $event->getParams());
-  }
-
-  public function preStmtExecute(Doctrine_Event $event)
-  {
-    $this->startQuery('SQL/query', $event->getQuery(), $event->getParams());
-  }
-
-  public function postFetch(Doctrine_Event $event)
-  {
-    $this->endQuery();
-  }
-
-  public function postStmtExecute(Doctrine_Event $event)
-  {
-    $this->endQuery();
-  }
-
-  private function startQuery($eventName, $query, $params)
-  {
-    $this->currentSpan = $this->agent->startSpan($eventName);
-
-    if ($this->currentSpan === null) {
-      return;
+    public function __construct(ScoutApmAgent $agent)
+    {
+        $this->agent = $agent;
     }
 
-    $this->currentSpan->tag('db.statement', $query);
-  }
-
-  private function endQuery()
-  {
-    if ($this->currentSpan === null) {
-      return;
+    public function preFetch(Doctrine_Event $event): void
+    {
+        $this->startQuery('SQL/query', $event->getQuery());
     }
 
-    $this->agent->stopSpan();
-    $this->currentSpan = null;
-  }
+    public function preStmtExecute(Doctrine_Event $event): void
+    {
+        $this->startQuery('SQL/query', $event->getQuery());
+    }
+
+    public function postFetch(Doctrine_Event $event): void
+    {
+        $this->endQuery();
+    }
+
+    public function postStmtExecute(Doctrine_Event $event): void
+    {
+        $this->endQuery();
+    }
+
+    private function startQuery(string $eventName, string $query): void
+    {
+        $this->currentSpan = $this->agent->startSpan($eventName);
+
+        if ($this->currentSpan === null) {
+            return;
+        }
+
+        $this->currentSpan->tag('db.statement', $query);
+    }
+
+    private function endQuery(): void
+    {
+        if ($this->currentSpan === null) {
+            return;
+        }
+
+        $this->agent->stopSpan();
+        $this->currentSpan = null;
+    }
 }
